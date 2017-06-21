7abec7c94b8f3908df8e8a6839eaaa991dba0730
zolivia <zolivia@zedat.fu-berlin.de>
2017-06-21 18:05:13 +0100

    Second attempt

---
+++ pyworkflow/em/packages/relion/protocol_angles_assign.py
@@ -0,0 +1,115 @@
+# **************************************************************************
+# *
+# * Authors:        Olivia Pfeil-Gardiner (zolivia@zedat.fu-berlin.de) [1]
+#                   J.M. De la Rosa Trevin (delarosatrevin@scilifelab.se) [2]
+# *
+# *  [1] OPIC, Division of Structural Biology, University of Oxford
+# *  [2] SciLifeLab, Stockholm University
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 2 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'scipion@cnb.csic.es'
+# *
+# **************************************************************************
+from pyworkflow.em.protocol.protocol_particles import ProtProcessParticles
+from pyworkflow.protocol.params import BooleanParam
+import pyworkflow.em as em
+from pyworkflow.em import metadata as md
+from convert import rowToAlignment, alignmentToRow
+from random import randint
+import numpy
+
+class ProtAnglesAssign(ProtProcessParticles):
+    """Allows the user to assign a tilt angle of 90 deg and randomize the rotational angle of particles"""
+    _label = 'assign angles'
+
+    def __init__(self, **kwargs):
+        ProtProcessParticles.__init__(self, **kwargs)
+
+    #--------------- DEFINE param functions ---------------
+
+    def _defineParams(self, form):
+
+        ProtProcessParticles._defineParams(self, form)
+
+    def _defineProcessParams(self,form):
+        form.addParam('assignTilt90', BooleanParam, label='Assign out of plane angle (Tilt) to 90 deg?',
+                      help='Assigns a value of 90 degrees to the tilt angle of each particle.')
+
+        form.addParam('randomizeRot', BooleanParam,
+                      label='Randomize the rotational angle around the particle axis (Rot)?',
+                      help='Assigns random values to the rotational angles around the particle axis.')
+
+    #--------------------------- INSERT steps functions --------------------------------------------
+
+    def _insertAllSteps(self):
+        self._insertFunctionStep('createOutputStep')
+
+    #--------------------------- STEPS functions ---------------------------------------------------
+
+    def createOutputStep(self):
+        particles = self.inputParticles.get()
+        partSet = self._createSetOfParticles()
+        #partSet.copyInfo(particles.getMicrographs())
+        if self.assignTilt90:
+            partSet.setAlignmentProj()
+            alignRow = md.Row()
+            for part in particles.iterItems():
+
+                #retrieving old alignment#
+             #  if particles.hasAlignment2D():
+              #      row2D = md.Row()
+              #      transform = part.getTransform()
+               #     row2D = alignmentToRow(transform, alignRow, em.ALIGN_2D)
+                #    ......alignRowpsi = row2Dpsi
+                #if particles.hasAlignmentProj():
+                #..................
+
+                alignRow.setValue(md.RLN_ORIENT_TILT, 90)
+                transform = rowToAlignment(alignRow, em.ALIGN_PROJ)
+                part.setTransform(transform)
+                partSet.append(part)
+
+        #Checking if the old SetOfParticles (particles) has the new alignment:
+        for part in particles.iterItems():
+            row = md.Row()
+            trans = part.getTransform()
+            alignmentToRow(trans, row, em.ALIGN_PROJ)
+            print row
+        #Checking if the new SetOfParticles (particles) has the new alignment:
+        for part in partSet.iterItems():
+            row = md.Row()
+            trans = part.getTransform()
+            alignmentToRow(trans, row, em.ALIGN_PROJ)
+            print row
+
+        #This part is not ready yet. If indeed we make a new set of particles (partSet)
+        #then I get the problem that I cannot iterate through the new set. (Why?)
+        #if self.randomizeRot:
+        #    partSet2 = self._createSetOfParticles()
+        #    partSet2.setAlignmentProj()
+        #    for part in particles.iterItems():
+        #        alignRow = md.Row()
+        #        alignRow.setValue(md.RLN_ORIENT_ROT, randint(-180,180))
+        #        transform = rowToAlignment(alignRow, em.ALIGN_PROJ)
+        #        part.setTransform(transform)
+        #        partSet2.append(part)
+
+
+
+        self._defineOutputs(outputParticles=partSet)
+        self._defineSourceRelation(self.inputParticles, partSet)
