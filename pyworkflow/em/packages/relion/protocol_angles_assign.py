ec7263a7bc35b6a40e4c6f4ed9e90f816bde644b
Jose Miguel de la Rosa Trevin <delarosatrevin@gmail.com>
2017-06-23 10:34:20 +0200

    Some code refactoring for assigning angles

--- pyworkflow/em/packages/relion/protocol_angles_assign.py
+++ pyworkflow/em/packages/relion/protocol_angles_assign.py
@@ -25,6 +25,7 @@
 # *  e-mail address 'scipion@cnb.csic.es'
 # *
 # **************************************************************************
+
 from pyworkflow.em.protocol.protocol_particles import ProtProcessParticles
 from pyworkflow.protocol.params import BooleanParam
 import pyworkflow.em as em
@@ -34,49 +35,74 @@ from random import randint
 import numpy
 
 class ProtAnglesAssign(ProtProcessParticles):
-    """Allows the user to assign a tilt angle of 90 deg and randomize the rotational angle of particles"""
+    """
+    Allows to assign a tilt angle of 90 deg and randomize
+    the rotational angle of particles.
+    """
     _label = 'assign angles'
 
     def __init__(self, **kwargs):
         ProtProcessParticles.__init__(self, **kwargs)
 
-    #--------------- DEFINE param functions ---------------
+    # --------------- DEFINE param functions ---------------
 
     def _defineParams(self, form):
 
         ProtProcessParticles._defineParams(self, form)
 
     def _defineProcessParams(self,form):
-        form.addParam('assignTilt90', BooleanParam, label='Assign out of plane angle (Tilt) to 90 deg?',
-                      help='Assigns a value of 90 degrees to the tilt angle of each particle.')
+        form.addParam('assignTilt90', BooleanParam, default=True,
+                      label='Assign out of plane angle (Tilt) to 90 deg?',
+                      help='Assigns a value of 90 degrees to the tilt angle '
+                           'of each particle.')
 
-        form.addParam('randomizeRot', BooleanParam,
-                      label='Randomize the rotational angle around the particle axis (Rot)?',
-                      help='Assigns random values to the rotational angles around the particle axis.')
+        form.addParam('randomizeRot', BooleanParam, default=True,
+                      label='Randomize the rotational angle around the '
+                            'particle axis (Rot)?',
+                      help='Assigns random values to the rotational angles '
+                           'around the particle axis.')
 
-    #--------------------------- INSERT steps functions --------------------------------------------
+    # ----------------- INSERT steps functions -----------------------------
 
     def _insertAllSteps(self):
         self._insertFunctionStep('createOutputStep')
 
-    #--------------------------- STEPS functions ---------------------------------------------------
+    # ------------------ STEPS functions -----------------------------------
 
     def createOutputStep(self):
-        particles = self.inputParticles.get()
-        partSet = self._createSetOfParticles()
+        inputParts = self.inputParticles.get()
+        outputParts = self._createSetOfParticles()
+        # Copy set properties from the input to the output set
+        outputParts.copyInfo(inputParts)
+        # Output particles will always have projection alignment
+        # since we are setting angles for initial helical reconstruction
+        outputParts.setAlignmentProj()
+        # Copy particles from input to output set, but we
+        # need to update each particle to set the new transformation
+        # with tilt=90 and rot randomized
+        outputParts.copyItems(inputParts,
+                              updateItemCallback=self._updateItemTransform)
+
+        self._defineOutputs(outputParticles=outputParts)
+        self._defineSourceRelation(self.inputParticles, outputParts)
+
+        return
+
+        # TODO: From here the code can be deleted after testing
+
         #partSet.copyInfo(particles.getMicrographs())
         if self.assignTilt90:
             partSet.setAlignmentProj()
             alignRow = md.Row()
-            for part in particles.iterItems():
+            for part in inputParts.iterItems():
 
                 #retrieving old alignment#
-                if particles.hasAlignment2D():
+                if inputParts.hasAlignment2D():
                     row2D = md.Row() #maybe unnecessary?
                     transform = part.getTransform()
                     alignmentToRow(transform, row2D, em.ALIGN_2D)
                     alignRow.copyFromRow(row2D)
-                if particles.hasAlignmentProj():
+                if inputParts.hasAlignmentProj():
                     rowProj = md.Row()
                     transform = part.getTransform()
                     alignmentToRow(transform, rowProj, em.ALIGN_PROJ)
@@ -100,7 +126,7 @@ class ProtAnglesAssign(ProtProcessParticles):
                 #partSet2.append(part)
 
         #Checking if the old SetOfParticles (particles) has the new alignment:
-        for part in particles.iterItems():
+        for part in inputParts.iterItems():
             row = md.Row()
             trans = part.getTransform()
             alignmentToRow(trans, row, em.ALIGN_PROJ)
@@ -116,3 +142,22 @@ class ProtAnglesAssign(ProtProcessParticles):
         
         self._defineOutputs(outputParticles=partSet)
         self._defineSourceRelation(self.inputParticles, partSet)
+
+    def _updateItemTransform(self, particle, row):
+        """ Set the tilt angle to 90 and randomize rot.
+        row is None in this case since we will not
+        iterate over any metadata file
+        """
+        alignRow = md.Row()
+        transform = particle.getTransform()
+        # ??? Is there any case that we should consider other alignment than 2D?
+        alignmentToRow(transform, alignRow, em.ALIGN_2D)
+
+        if self.assignTilt90:
+            alignRow.setValue(md.RLN_ORIENT_TILT, 90)
+
+        if self.randomizeRot:
+            alignRow.setValue(md.RLN_ORIENT_TILT, randint(0, 359))
+
+        transform = rowToAlignment(alignRow, em.ALIGN_PROJ)
+        particle.setTransform(transform)
