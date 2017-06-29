f2640b5d43d2339543fa3b9fd73e4a4ecfbd7d97
zolivia <zolivia@zedat.fu-berlin.de>
2017-06-29 17:30:54 +0100

    saved bimodalPriors with particle

--- pyworkflow/em/packages/relion/protocol_extract_particles.py
+++ pyworkflow/em/packages/relion/protocol_extract_particles.py
@@ -367,6 +367,10 @@ class ProtRelionExtractParticles(em.ProtExtractParticles, ProtRelionBase):
 
                     p.setTransform(transform)
                     p.trackLength = Float(coord.trackLength.get())
+                    p.bimodalPriors = Float(coord.bimodalPriors.get())
+                    #Should we print an error message incase particles come with filaments but without trackLength
+                    # or bimodalPriors param?
+                    #(shouldn't really happen though...)
 
                 partSet.append(p)
                 count += 1
